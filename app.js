3a0b43a6c22ad0726cb2593f86fb7efb7058e12f
Anto Mac <antomac@mail.com>
2021-10-31 22:08:46 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const quote = document.getElementById('quote')
+const author = document.getElementById('author')
+const quoteBtn = document.getElementById('quoteBtn')
+
+quoteBtn.addEventListener('click', generateQuote)
+
+
+generateQuote()
+
+
+function generateQuote() {
+
+    const config = ({
+        headers: {
+            'Accept': 'application/json' //Accept = Key, App/json = value.
+        }
+    })
+
+
+    fetch('https://api.quotable.io/random', config)
+        .then((response) => response.json())
+        .then((data) => {
+            quote.innerHTML = `"${data.content}"`;
+            author.innerHTML = data.author;
+        })
+
+
+
+}
